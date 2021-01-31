97a53af93e96cc598bd9ce1b480c1f7a2668250c
akshatha-KA <kumariakshatha6@gmail.com>
2021-01-31 20:49:18 +0530

    draft

--- addResponse.php
+++ addResponse.php
@@ -1,51 +1,42 @@
+<html>
+    <body>
 <?php
-header('Access-Control-Allow-Origin','*');
-$informationMessage =""; // Update the status or any kind of query
-$requestData = "";// used to get data from the Frontend
-$responseData = new \stdClass();
-$databaseConnectionStatus = "";
-$databaseMessage = "";
+  $nformationMessage ="";
+  $requestData="";
+  $responseData= new \stdClass();
+  $databaseConnectionStatus ="";
+  $databaseMessage="";
 
-$serverName ="localhost";
-$databaseName ="portfolio";
-$tableName = "portfoliowebsite";
-$username ="root";
-$password = "";
-if(isset($_POST)){
-    $Name = $_POST['Name'];
-    $Email = $_POST['Email'];
-    $Phone = $_POST['Phone'];
+  $Name= $_POST['Name'];
+  $Email= $_POST['Email'];
+  $Phone= $_POST['Phone'];
 
-    $connection = new mysqli($serverName,$username,$password,$databaseName);
+  $servername = "localhost";
+  $database = "portfolio";
+  $tablename="portfoliowebsite";
+  $username = "root";
+  $password="";
 
-    if($connection->connect_errno){
-        $databaseMessage = $connection->connect_error;
-        $databaseConnectionStatus = "false";
-        $informationMessage="500 Internal Server Error";
-    }else{
-        $databaseMessage = $connection->connect_error;
-        $databaseConnectionStatus = "true";
-
-       $insert = "INSERT INTO $tableName VALUES ('','$Name','$Email','$Phone')";
-       if($connection->query($insert)){
-        $informationMessage="Data Added Successfully";
-       }else{
-        $informationMessage="Error";
-       }
-    }
-}else{
-    $informationMessage = "No Data";
-    $requestData = "Wrong Path";
-    $databaseConnectionStatus ="false";
-    $databaseMessage ="Check the path";
-}
-
-$responseData->status = $databaseConnectionStatus;
-$responseData->msg = $databaseMessage;
-$responseData->info = $informationMessage;
-$responseData->data = "Null";
-
-echo json_encode($responseData,JSON_PRETTY_PRINT);
-
-
+  $connection = new mysqli($servername,$username,$password,$database);
+  if($connection->connect_errno){
+      $databaseMessage= $connection ->connect_error;
+      $databaseConnectionStatus="false";
+      $informationMessage= "500 Internal Server Error";
+     }
+  else
+     {
+        $insert = "INSERT INTO $tablename VALUES('','$Name','$Email','$Phone')";
+      if($connection->query($insert)){
+          $informationMessage = "Data Successfully added";
+     }
+      else
+      {
+          $informationMessage= "Error";
+      }
+  
+  }
+  header('Location:contact.html');
+  exit;
 ?>
+    </body>
+</html>
